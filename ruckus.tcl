d6c5407f67418b183d3cc073622ff36a32dc7cd6
Till Straumann <strauman@slac.stanford.edu>
2019-07-22 14:18:28 -0700

    ruckus.tcl: accept surf >= 1.9.9 (not only 1.9.9)

--- ruckus.tcl
+++ ruckus.tcl
@@ -3,7 +3,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-   if { [SubmoduleCheck {surf}             {1.9.9}  "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {1.9.9}] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
